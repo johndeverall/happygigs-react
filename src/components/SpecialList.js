169d06bbb8052053f5b3030ba4cf438c14f79222
John Deverall <johndeverall@gmail.com>
2021-11-30 22:29:21 +1300

    Add test data

--- src/components/SpecialList.js
+++ src/components/SpecialList.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Special from "./Special";
+import SpecialServiceClient from "../spi/SpecialServiceClient"
+
+
+
+
 
 function SpecialList(){
     return (
+
         <Special name="a name" description="one" startTime="two" endTime="three" latitude="" longitude="" />
     );
 }
---
+++ src/spi/SpecialServiceClient.ts
@@ -0,0 +1,329 @@
+
+export const data = {
+    "data": {
+        "specials": [
+            {
+                "description": "House drinks from $7.50",
+                "venue": {
+                    "name": "Coco's Bar & Grill",
+                    "externalMapLink": "https://www.google.com/maps/place/COCO'S+BAR+%26+GRILL/@-41.2866157,174.7747551,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd443d16643:0x3b21ad1241cc8861!8m2!3d-41.2866157!4d174.7769438",
+                    "venueWebsite": "http://www.cocosbarandgrill.co.nz/",
+                    "address": {
+                        "streetName": "Willeston Street"
+                    }
+                },
+                "startTime": "11:45:00",
+                "finishTime": "22:00:00",
+                "timeToInMinutes": 2.133061416785995
+            },
+            {
+                "description": "Burger & a Pint from $19.50",
+                "venue": {
+                    "name": "Coco's Bar & Grill",
+                    "externalMapLink": "https://www.google.com/maps/place/COCO'S+BAR+%26+GRILL/@-41.2866157,174.7747551,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd443d16643:0x3b21ad1241cc8861!8m2!3d-41.2866157!4d174.7769438",
+                    "venueWebsite": "http://www.cocosbarandgrill.co.nz/",
+                    "address": {
+                        "streetName": "Willeston Street"
+                    }
+                },
+                "startTime": "11:45:00",
+                "finishTime": "22:00:00",
+                "timeToInMinutes": 2.133061416785995
+            },
+            {
+                "description": "25 dollar burger and bier",
+                "venue": {
+                    "name": "Munchen Food Hall and Bier Haus",
+                    "externalMapLink": "https://www.google.com/maps/place/Munchen:+Food+Hall+%26+Bier+Haus/@-41.2850653,174.7766867,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd37d9f499b:0x7f8ed7f7c27e847b!8m2!3d-41.2850653!4d174.7788754",
+                    "venueWebsite": "https://www.whg.co.nz/",
+                    "address": {
+                        "streetName": "Queens Wharf"
+                    }
+                },
+                "startTime": "11:00:00",
+                "finishTime": "23:00:00",
+                "timeToInMinutes": 2.73972494508042
+            },
+            {
+                "description": "Chicken wrap for $10.00",
+                "venue": {
+                    "name": "Ibis Hotel",
+                    "externalMapLink": "https://www.google.com/maps/place/ibis+Wellington/@-41.2837453,174.7746773,17z/data=!3m1!4b1!4m8!3m7!1s0x6d38afd4c2e668ad:0x2a41baf2d289b1cf!5m2!4m1!1i2!8m2!3d-41.2837453!4d174.776866",
+                    "venueWebsite": "https://www.accorhotels.com/gb/hotel-3049-ibis-wellington/index.shtml?utm_term=mar&gclid=CjwKCAiAlO7uBRANEiwA_vXQ-7xO-n7GjZXm7kXziHuALSY0X-BEY6pxrlgkMFkZ4DNPSegMFJXsdhoC-kIQAvD_BwE&utm_campaign=ppc-ibi-mar-goo-nz-en-nz-mix-sear-nz&utm_medium=cpc&utm_content=nz-en-NZ-V5704&utm_source=google",
+                    "address": {
+                        "streetName": "Featherston Street"
+                    }
+                },
+                "startTime": "11:30:00",
+                "finishTime": "14:00:00",
+                "timeToInMinutes": 5.634209478131471
+            },
+            {
+                "description": "25 dollar Burger, fries and beer for $25",
+                "venue": {
+                    "name": "D4",
+                    "externalMapLink": "https://www.google.com/maps/place/D4+on+Featherston/@-41.28332,174.7749693,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd4c9100563:0x42f8e72ec583fe40!8m2!3d-41.28332!4d174.777158",
+                    "venueWebsite": "https://www.d4featherston.co.nz/",
+                    "address": {
+                        "streetName": "Featherston Street"
+                    }
+                },
+                "startTime": "16:00:00",
+                "finishTime": "21:00:00",
+                "timeToInMinutes": 6.253764395392418
+            },
+            {
+                "description": "$9.50 Weekday breakfast special",
+                "venue": {
+                    "name": "Leuven",
+                    "externalMapLink": "https://www.google.com/maps/place/Leuven+Belgium+Beer+Cafe/@-41.2831569,174.7751113,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd4b56dde99:0xb4c3e32a57a73dcb!8m2!3d-41.2831569!4d174.7773",
+                    "venueWebsite": "http://www.leuven.co.nz/",
+                    "address": {
+                        "streetName": "Featherston Street"
+                    }
+                },
+                "startTime": "07:00:00",
+                "finishTime": "11:00:00",
+                "timeToInMinutes": 6.498920054519149
+            },
+            {
+                "description": "20 dollar Burger and Beer",
+                "venue": {
+                    "name": "POP",
+                    "externalMapLink": "https://www.google.com/maps/place/POP+Wellington/@-41.2888335,174.7725439,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38af06d9123249:0x6f1be7d117e5bff!8m2!3d-41.2888335!4d174.7747326",
+                    "venueWebsite": "https://popbar.co.nz/",
+                    "address": {
+                        "streetName": "Willis Street"
+                    }
+                },
+                "startTime": "11:00:00",
+                "finishTime": "22:00:00",
+                "timeToInMinutes": 6.951831833139382
+            },
+            {
+                "description": "2 for 1 original slices ",
+                "venue": {
+                    "name": "Tommy Millions",
+                    "externalMapLink": "https://www.google.com/maps/place/TOMMY+MILLIONS+-+Featherston+Street/@-41.2829456,174.7747346,17z/data=!4m12!1m6!3m5!1s0x6d38ae2b34a9facf:0x58ab096046156a4e!2sTOMMY+MILLIONS+-+Featherston+Street!8m2!3d-41.2829456!4d174.7769233!3m4!1s0x6d38ae2b34a9facf:0x58ab096046156a4e!8m2!3d-41.2829456!4d174.7769233",
+                    "venueWebsite": "https://www.tommymillions.co.nz/",
+                    "address": {
+                        "streetName": "Featherston Street"
+                    }
+                },
+                "startTime": "11:00:00",
+                "finishTime": "16:00:00",
+                "timeToInMinutes": 7.035073668219479
+            },
+            {
+                "description": "Happy Hour: 12 dumplings, any cocktail for $17",
+                "venue": {
+                    "name": "Dumpling'd",
+                    "externalMapLink": "https://www.google.com/maps/place/Dumpling'd/@-41.28901,174.7722509,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd615637407:0xbe5e62ac1224ccb2!8m2!3d-41.28901!4d174.7744396",
+                    "venueWebsite": "https://dumplingd.co.nz/",
+                    "address": {
+                        "streetName": "Boulcott Street"
+                    }
+                },
+                "startTime": "16:00:00",
+                "finishTime": "17:30:00",
+                "timeToInMinutes": 7.489446336156344
+            },
+            {
+                "description": "6 dollar Salt & Pepper Squid",
+                "venue": {
+                    "name": "The George ",
+                    "externalMapLink": "https://www.google.com/maps/place/The+St+George+Bar+%26+Restaurant/@-41.2892362,174.7722862,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd63fad350d:0xe116630137711bb8!8m2!3d-41.2892362!4d174.7744749",
+                    "venueWebsite": "https://www.georgeonwillis.com/",
+                    "address": {
+                        "streetName": "Willis Street"
+                    }
+                },
+                "startTime": "11:00:00",
+                "finishTime": "23:59:00",
+                "timeToInMinutes": 7.739119999190852
+            },
+            {
+                "description": "Happy Hour",
+                "venue": {
+                    "name": "The George",
+                    "externalMapLink": "https://www.google.com/maps/place/The+St+George+Bar+%26+Restaurant/@-41.2892362,174.7722862,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd63fad350d:0xe116630137711bb8!8m2!3d-41.2892362!4d174.7744749",
+                    "venueWebsite": "https://www.georgeonwillis.com/",
+                    "address": {
+                        "streetName": "Willis Street"
+                    }
+                },
+                "startTime": "17:00:00",
+                "finishTime": "19:00:00",
+                "timeToInMinutes": 7.739119999190852
+            },
+            {
+                "description": "Lunch from $12 with free coffee",
+                "venue": {
+                    "name": "The George",
+                    "externalMapLink": "https://www.google.com/maps/place/The+St+George+Bar+%26+Restaurant/@-41.2892362,174.7722862,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd63fad350d:0xe116630137711bb8!8m2!3d-41.2892362!4d174.7744749",
+                    "venueWebsite": "https://www.georgeonwillis.com/",
+                    "address": {
+                        "streetName": "Willis Street"
+                    }
+                },
+                "startTime": "11:00:00",
+                "finishTime": "14:30:00",
+                "timeToInMinutes": 7.739119999190852
+            },
+            {
+                "description": "Sushi on sale $0.9 each",
+                "venue": {
+                    "name": "Ki Sushi",
+                    "externalMapLink": "https://www.google.com/maps/place/Ki+Sushi/@-41.2821354,174.7756741,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38ae2b44b7698f:0x69f68c617f692a5f!8m2!3d-41.2821354!4d174.7778628",
+                    "venueWebsite": "https://www.tripadvisor.co.nz/Restaurant_Review-g255115-d15757315-Reviews-KI_Sushi-Wellington_Greater_Wellington_North_Island.html",
+                    "address": {
+                        "streetName": "Featherston Street"
+                    }
+                },
+                "startTime": "15:00:00",
+                "finishTime": "17:00:00",
+                "timeToInMinutes": 8.28381256623896
+            },
+            {
+                "description": "Any salad with a glass of house wine",
+                "venue": {
+                    "name": "Dillinger's Brasserie & Bar",
+                    "externalMapLink": "https://www.google.com/maps/place/Dillingers+Brasserie+%26+Bar/@-41.2822364,174.7743237,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38ae2b2809ba0d:0x481af3615a316dc5!8m2!3d-41.2822364!4d174.7765124",
+                    "venueWebsite": "https://www.dillingers.co.nz/",
+                    "address": {
+                        "streetName": "Lambton Quay"
+                    }
+                },
+                "startTime": "16:00:00",
+                "finishTime": "21:00:00",
+                "timeToInMinutes": 8.50327882820469
+            },
+            {
+                "description": "12 dollar lunch special (one main, one rice, one bread)",
+                "venue": {
+                    "name": "Great India",
+                    "externalMapLink": "https://www.google.com/maps/place/Great+India+Restaurant/@-41.2920888,174.7764878,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd74b496317:0x5584ed08277648c5!8m2!3d-41.2920888!4d174.7786765",
+                    "venueWebsite": "http://www.greatindia.co.nz/",
+                    "address": {
+                        "streetName": "Manners Street"
+                    }
+                },
+                "startTime": "12:00:00",
+                "finishTime": "14:00:00",
+                "timeToInMinutes": 10.734058078818265
+            },
+            {
+                "description": "Happy Hours (selected beer, wine and spirts)",
+                "venue": {
+                    "name": "The Residence Bar & Kitchen",
+                    "externalMapLink": "https://www.google.com/maps/place/The+Residence/@-41.2926147,174.7773996,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd0a47be735:0xda560924726bf396!8m2!3d-41.2926147!4d174.7795883",
+                    "venueWebsite": "https://www.residencebar.co.nz/",
+                    "address": {
+                        "streetName": "Courtenay Place"
+                    }
+                },
+                "startTime": "17:00:00",
+                "finishTime": "19:00:00",
+                "timeToInMinutes": 11.849190715135576
+            },
+            {
+                "description": "Pizza night",
+                "venue": {
+                    "name": "Danger Danger",
+                    "externalMapLink": "https://yyyyyyyyyyx/",
+                    "venueWebsite": "http://www.dangerdanger.co.nz/",
+                    "address": {
+                        "streetName": "Courtenay Place"
+                    }
+                },
+                "startTime": "19:00:00",
+                "finishTime": "22:00:00",
+                "timeToInMinutes": 13.582094365584597
+            },
+            {
+                "description": "10 dollar Chicken Katsu Don",
+                "venue": {
+                    "name": "Sen Japanese Restaurant & Bar",
+                    "externalMapLink": "https://www.google.com/maps/place/SEN+Japanese+Restaurant/@-41.2934126,174.778442,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38af10045e91a3:0x75982e9d51bbfccf!8m2!3d-41.2934126!4d174.7806307",
+                    "venueWebsite": "https://www.facebook.com/restaurant.sen/",
+                    "address": {
+                        "streetName": "Courtenay Place"
+                    }
+                },
+                "startTime": "11:00:00",
+                "finishTime": "14:30:00",
+                "timeToInMinutes": 13.62460955996785
+            },
+            {
+                "description": "2 for 1 deserts",
+                "venue": {
+                    "name": "The Library Bar",
+                    "externalMapLink": "https://www.google.com/maps/place/The+Library/@-41.2935914,174.7790823,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afda87acd8f9:0x4e7754972cde3082!8m2!3d-41.2935914!4d174.781271",
+                    "venueWebsite": "https://www.thelibrary.co.nz/",
+                    "address": {
+                        "streetName": "Courtenay Place"
+                    }
+                },
+                "startTime": "17:00:00",
+                "finishTime": "23:00:00",
+                "timeToInMinutes": 14.192636228701383
+            },
+            {
+                "description": "8 dollar Wines (Sav, Rose, Cotes du Rhone)",
+                "venue": {
+                    "name": "Le Samourai",
+                    "externalMapLink": "https://www.google.com/maps/place/Le+Samourai/@-41.2939049,174.7790245,17z/data=!4m12!1m6!3m5!1s0x6d38afda62cdfd49:0xed4772e9e6a1057e!2sLe+Samourai!8m2!3d-41.2939049!4d174.7812132!3m4!1s0x6d38afda62cdfd49:0xed4772e9e6a1057e!8m2!3d-41.2939049!4d174.7812132",
+                    "venueWebsite": "http://lesamourai.co.nz/",
+                    "address": {
+                        "streetName": "Tory Street"
+                    }
+                },
+                "startTime": "15:00:00",
+                "finishTime": "18:00:00",
+                "timeToInMinutes": 14.743247244025618
+            },
+            {
+                "description": "8 dollar lunch special - Kebab or Snack Pack",
+                "venue": {
+                    "name": "Kebabs on Queen",
+                    "externalMapLink": "https://www.google.com/maps/place/Kebabs+on+Queen/@-41.282134,174.7603532,14z/data=!4m8!1m2!2m1!1skebabs+on+queen!3m4!1s0x0:0x93f87950b175f757!8m2!3d-41.2930678!4d174.7794235",
+                    "venueWebsite": "http://kebabsonqueen.co.nz/",
+                    "address": {
+                        "streetName": "Courtenay Place"
+                    }
+                },
+                "startTime": "09:00:00",
+                "finishTime": "15:00:00",
+                "timeToInMinutes": 24.191513699061634
+            },
+            {
+                "description": "$8.00 All day breakfast menu",
+                "venue": {
+                    "name": "The Grand",
+                    "externalMapLink": "https://www.google.com/maps/place/The+Grand+Steakhouse/@-41.2952059,174.7612294,14z/data=!4m8!1m2!2m1!1sthe+grand!3m4!1s0x6d38afda7768c7df:0x866999bd3d963e53!8m2!3d-41.2933205!4d174.7804224",
+                    "venueWebsite": "http://thegrandwellington.com/",
+                    "address": {
+                        "streetName": "Courtenay Place"
+                    }
+                },
+                "startTime": "09:00:00",
+                "finishTime": "12:00:00",
+                "timeToInMinutes": 27.19315528437948
+            },
+            {
+                "description": "12 dollar express lunch menu",
+                "venue": {
+                    "name": "The Grand",
+                    "externalMapLink": "https://www.google.com/maps/place/The+Grand+Steakhouse/@-41.2952059,174.7612294,14z/data=!4m8!1m2!2m1!1sthe+grand!3m4!1s0x6d38afda7768c7df:0x866999bd3d963e53!8m2!3d-41.2933205!4d174.7804224",
+                    "venueWebsite": "http://thegrandwellington.com/",
+                    "address": {
+                        "streetName": "Courtenay Place"
+                    }
+                },
+                "startTime": "11:00:00",
+                "finishTime": "17:00:00",
+                "timeToInMinutes": 27.19315528437948
+            }
+        ]
+    }
+}
