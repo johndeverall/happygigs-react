5c425e468ce22e48ce3250cbf4c6e9966bd55ffb
unknown <jasonk6243@gmail.com>
2021-11-30 21:47:38 +1300

    Adding Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import SpecialList from "./SpecialList";
+import Footer from "./Footer";
 
 function App(){
   return (
       <React.Fragment>
         <Header/>
         <SpecialList/>
+        <Footer/>
       </React.Fragment>
   );
 }
