40e4ec950b8a8214f9fb16e37cc34395add1004e
John Deverall <johndeverall@gmail.com>
2021-12-29 22:42:57 +1300

    Add call to get dummy specials

--- src/components/SpecialList.js
+++ src/components/SpecialList.js
@@ -4,15 +4,12 @@ import SpecialServiceClient from "../spi/SpecialServiceClient"
 
 
 
-
-
 function SpecialList(){
     return (
         <React.Fragment>
 
             <hr/>
             {SpecialServiceClient.specials.map(special =>
-
                 <Special description={special.description} startTime={special.startTime} finishTime={special.finishTime} latitude={special.latitude} longitude={special.longitude} />
             )}
 
